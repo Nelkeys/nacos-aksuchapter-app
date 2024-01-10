3794c8fce8ecd656312bcba945e7231a4f87f698
Nelkeys <nelsonekoh212@gmail.com>
2024-01-10 16:37:31 +0100

    Loader Added to all pages

---
+++ js/loader.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate loading process
+    let percentageElement = document.getElementById("percentage");
+    let contentElement = document.querySelector(".content");
+  
+    let percentage = 0;
+    let interval = setInterval(function () {
+      percentageElement.innerText = `${percentage}%`;
+      percentage++;
+  
+      if (percentage > 100) {
+        clearInterval(interval);
+        // Hide loader and show content
+        document.querySelector(".loader-container").style.display = "none";
+        contentElement.style.display = "block";
+      }
+    }, 20);
+});
+  
+
+
+function reloadHomePage() {
+    window.location.reload();
+}
