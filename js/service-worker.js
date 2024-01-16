a954be887774e57fcd62048b676e518680adb1c2
Nelkeys <nelsonekoh212@gmail.com>
2024-01-16 22:12:29 +0100

    offline ability

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,32 @@
+// service-worker.js
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/index.css',
+    '/css/general/styles.css',
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-    console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-    console.error('Service Worker registration failed:', error);
-    });
-}
+    '/materials.html',
+    '/css/materials.css',
 
+    '/notifications.html',
+    '/css/notifications.css',
+
+    '/no-data.html',
+    '/css/no-data.css',
+    // Add other static assets you want to cache
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
