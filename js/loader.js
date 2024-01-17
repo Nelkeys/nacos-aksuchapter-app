e703bce2c24f55e7ff28d1397c938ab8f0095c2c
Nelkeys <nelsonekoh212@gmail.com>
2024-01-17 14:47:01 +0100

    new loader

--- js/loader.js
+++ js/loader.js
@@ -1,27 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var pageLinks = document.querySelectorAll('.page-link');
-    
-    pageLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            showLoader();
-
-            var targetPage = event.target.getAttribute('href');
-            console.log('Target Page:', targetPage); // Add this line for debugging
-
-            // Wait for the loader to be displayed before navigating
-            setTimeout(function () {
-                hideLoader();
-                window.location.href = targetPage;
-            }, 0);
-        });
-    });
-});
-
-function showLoader() {
+function loadPage(page) {
+    // Display the loader
     document.getElementById('loader').style.display = 'block';
-}
 
-function hideLoader() {
+    // Hide the loader immediately and load the new page
     document.getElementById('loader').style.display = 'none';
+    window.location.href = page;
 }
