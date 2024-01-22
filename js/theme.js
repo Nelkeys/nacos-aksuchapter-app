3751ac43ac863494786651fc9966cc1ae7e3084d
Nelkeys <nelsonekoh212@gmail.com>
2024-01-22 21:36:57 +0100

    Dark mode

---
+++ js/theme.js
@@ -0,0 +1,35 @@
+// JavaScript (script.js)
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.add('dark-mode');
+    document.getElementById('moon').style.display = 'none';
+    document.getElementById('sun').style.display = 'inline-block';
+
+    // Save user preference to localStorage
+    localStorage.setItem('theme', 'dark');
+}
+
+// Function to toggle light mode
+function toggleLightMode() {
+    document.body.classList.remove('dark-mode');
+    document.getElementById('moon').style.display = 'inline-block';
+    document.getElementById('sun').style.display = 'none';
+
+    // Save user preference to localStorage
+    localStorage.setItem('theme', 'light');
+}
+
+// Function to set the theme based on user preference in localStorage
+function setThemeFromLocalStorage() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+        toggleDarkMode();
+    } else {
+        toggleLightMode();
+    }
+}
+
+// Set the theme when the page loads
+document.addEventListener('DOMContentLoaded', setThemeFromLocalStorage);
