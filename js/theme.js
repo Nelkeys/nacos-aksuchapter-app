7ead87410f69b0a9db03057e01156aed5b8a17fb
Nelkeys <nelsonekoh212@gmail.com>
2024-01-23 08:12:18 +0100

    meta thheme

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,10 @@
-// JavaScript (script.js)
 
 // Function to toggle dark mode
 function toggleDarkMode() {
     document.body.classList.add('dark-mode');
     document.getElementById('moon').style.display = 'none';
     document.getElementById('sun').style.display = 'inline-block';
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#222');
 
     // Save user preference to localStorage
     localStorage.setItem('theme', 'dark');
@@ -15,6 +15,7 @@ function toggleLightMode() {
     document.body.classList.remove('dark-mode');
     document.getElementById('moon').style.display = 'inline-block';
     document.getElementById('sun').style.display = 'none';
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#fff');
 
     // Save user preference to localStorage
     localStorage.setItem('theme', 'light');
