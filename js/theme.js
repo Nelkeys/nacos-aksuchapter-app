ab01f4aff59706e0bc4b666186695ebe1df227d8
Nelkeys <nelsonekoh212@gmail.com>
2024-01-25 23:28:26 +0100

    hey

--- js/theme.js
+++ js/theme.js
@@ -1,28 +1,25 @@
 // Function to toggle dark mode
 function toggleDarkMode() {
+    // Change theme color in <meta> tag
+    document.querySelector('.theme-color-stuff').setAttribute('content', '#222');
     document.body.classList.add('dark-mode');
     document.getElementById('moon').style.display = 'none';
     document.getElementById('sun').style.display = 'inline-block';
 
     // Save user preference to localStorage
     localStorage.setItem('theme', 'dark');
-
-    // Change theme color in <meta> tag
-    document.querySelector('.theme-color-stuff').setAttribute('content', '#222');
 }
 
 // Function to toggle light mode
 function toggleLightMode() {
+    // Change theme color in <meta> tag
+    document.querySelector('.theme-color-stuff').setAttribute('content', '#fff');
     document.body.classList.remove('dark-mode');
     document.getElementById('moon').style.display = 'inline-block';
     document.getElementById('sun').style.display = 'none';
 
     // Save user preference to localStorage
     localStorage.setItem('theme', 'light');
-
-    // Change theme color in <meta> tag
-    document.querySelector('.theme-color-stuff').setAttribute('content', '#fff');
-
 }
 
 // Function to set the theme based on user preference in localStorage
