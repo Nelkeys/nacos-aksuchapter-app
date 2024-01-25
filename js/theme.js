1fa87345bc0c4f26ba681fdc74730b42cba736f6
Nelkeys <nelsonekoh212@gmail.com>
2024-01-25 12:06:14 +0100

    theme

--- js/theme.js
+++ js/theme.js
@@ -1,27 +1,21 @@
-
-// Function to toggle dark mode
 function toggleDarkMode() {
     document.body.classList.add('dark-mode');
     document.getElementById('moon').style.display = 'none';
     document.getElementById('sun').style.display = 'inline-block';
     document.querySelector('meta[name="theme-color"]').setAttribute('content', '#222');
 
-    // Save user preference to localStorage
     localStorage.setItem('theme', 'dark');
 }
 
-// Function to toggle light mode
 function toggleLightMode() {
     document.body.classList.remove('dark-mode');
     document.getElementById('moon').style.display = 'inline-block';
     document.getElementById('sun').style.display = 'none';
     document.querySelector('meta[name="theme-color"]').setAttribute('content', '#fff');
 
-    // Save user preference to localStorage
     localStorage.setItem('theme', 'light');
 }
 
-// Function to set the theme based on user preference in localStorage
 function setThemeFromLocalStorage() {
     const savedTheme = localStorage.getItem('theme');
 
@@ -32,5 +26,14 @@ function setThemeFromLocalStorage() {
     }
 }
 
-// Set the theme when the page loads
 document.addEventListener('DOMContentLoaded', setThemeFromLocalStorage);
+
+// Apply the theme to dynamically loaded content
+const observer = new MutationObserver(() => {
+    setThemeFromLocalStorage();
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
