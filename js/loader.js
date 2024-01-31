a742f3d56015578ce5fc36d17fb6264453d71727
Nelkeys <nelsonekoh212@gmail.com>
2024-01-31 20:22:24 +0100

    test loader style

--- js/loader.js
+++ js/loader.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add a small delay before hiding the loader (adjust the time as needed)
     hideLoaderTimeout = setTimeout(function () {
         hideLoader();
-    },0); // Adjust the time as needed
+    },2000); // Adjust the time as needed
 });
 
 function showLoader() {
