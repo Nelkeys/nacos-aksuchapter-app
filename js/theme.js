57d726c2dadc0bdba3f195c5093c2dcc635f9a3d
dhelafrank <ubongabasijerome8@gmail.com>
2024-02-07 17:19:52 +0100

    Instance: Theme Flash Bug Fix

--- js/theme.js
+++ js/theme.js
@@ -1,37 +1,37 @@
 // Function to toggle dark mode
 function toggleDarkMode() {
-    document.querySelector('.theme-color-stuff').setAttribute('content', '#01020A');
+    if ((document.title).includes("Home")) {
 
+        //Not the best practice; but help mitigate the errors
+        //by chechking the screens current title
+        document.querySelector('.theme-color-stuff').setAttribute('content', '#01020A');
+
+        document.getElementById('moon').style.display = 'none';
+        document.getElementById('sun').style.display = 'inline-block';
+
+        localStorage.setItem('theme', 'dark');
+    }
     document.body.classList.add('dark-mode');
-    document.getElementById('moon').style.display = 'none';
-    document.getElementById('sun').style.display = 'inline-block';
-
-    localStorage.setItem('theme', 'dark');
 }
 
 function toggleLightMode() {
-    document.querySelector('.theme-color-stuff').setAttribute('content', '#fff');
+    if ((document.title).includes("Home")) {
+        document.querySelector('.theme-color-stuff').setAttribute('content', '#fff');
 
+        document.getElementById('moon').style.display = 'inline-block';
+        document.getElementById('sun').style.display = 'none';
+
+        localStorage.setItem('theme', 'light');
+    }
     document.body.classList.remove('dark-mode');
-    document.getElementById('moon').style.display = 'inline-block';
-    document.getElementById('sun').style.display = 'none';
-
-    localStorage.setItem('theme', 'light');
 }
 
 // Function to set the theme based on user preference in localStorage
 function setThemeFromLocalStorage() {
     const savedTheme = localStorage.getItem('theme');
-
     if (savedTheme === 'dark') {
         toggleDarkMode();
     } else {
         toggleLightMode();
     }
 }
-
-
-// Add the event listener after the DOM content has loaded
-document.addEventListener('DOMContentLoaded', function () {
-    setThemeFromLocalStorage();
-});
