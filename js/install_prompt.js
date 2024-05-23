0c6910d5e6e50fa3ebebb68de876cdd100d42429
Nelkeys <nelsonekoh212@gmail.com>
2024-05-23 00:45:48 +0000

    Update README.md file

--- js/install_prompt.js
+++ js/install_prompt.js
@@ -1,14 +1,16 @@
 // Check if the browser supports service workers
 if ('serviceWorker' in navigator) {
-    // Register the service worker
-    navigator.serviceWorker.register('/js/.service-worker.js')
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
         .catch(error => {
             console.error('Service Worker registration failed:', error);
         });
+    });
 }
+
 
 // Check if the app is already installed
 window.addEventListener('beforeinstallprompt', (event) => {
